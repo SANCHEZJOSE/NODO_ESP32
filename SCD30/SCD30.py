f9cf3d45ae45632f1259c35b9cf28dfe2829f58d
BrionesMathias <mathii.briones@gmail.com>
2019-04-10 16:39:17 -0400

    modificaciones

--- SCD30/SCD30.py
+++ SCD30/SCD30.py
@@ -1,13 +1,15 @@
+from micropython import constans
 from constans import *
 class SCD30:
   def __init__(self):
     devAddr = SCD30_I2C_ADDRESS;
-  def SCD30.initialize():
+  def initialize():
     #Set temperature offset。
     #setTemperatureOffset(0);  
     setMeasurementInterval(2) # 2 seconds between measurements
     startPeriodicMeasurment() # start periodic measuments
     #setAutoSelfCalibration(true); # Enable auto-self-calibration
-  def SCD30.setTemperatureOffset(offset):
+  def setTemperatureOffset(offset):
     writeCommandWithArguments(SCD30_SET_TEMP_OFFSET, offset)
+
 
