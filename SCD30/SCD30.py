ab76bae06d043589af2e359f99cab204df4198c2
BrionesMathias <mathii.briones@gmail.com>
2019-04-10 17:06:19 -0400

    modificaciones2

--- SCD30/SCD30.py
+++ SCD30/SCD30.py
@@ -2,7 +2,7 @@ from micropython import constans
 from constans import *
 class SCD30:
   def __init__(self):
-    devAddr = SCD30_I2C_ADDRESS;
+    devAddr = SCD30_I2C_ADDRESS
   def initialize():
     #Set temperature offset。
     #setTemperatureOffset(0);  
@@ -11,5 +11,19 @@ class SCD30:
     #setAutoSelfCalibration(true); # Enable auto-self-calibration
   def setTemperatureOffset(offset):
     writeCommandWithArguments(SCD30_SET_TEMP_OFFSET, offset)
+  def isAvailable():
+   return readRegister(SCD30_GET_DATA_READY)
+  def setAutoSelfCalibration(enable):
+    if(enable):
+      writeCommandWithArguments(SCD30_AUTOMATIC_SELF_CALIBRATION, 1) #Activate continuous ASC
+    else: 
+      writeCommandWithArguments(SCD30_AUTOMATIC_SELF_CALIBRATION, 0) #Deactivate continuous ASC
+  def setMeasurementInterval(interval):
+      writeCommandWithArguments(SCD30_SET_MEASUREMENT_INTERVAL, interval)
+  def startPeriodicMeasurment():
+     writeCommandWithArguments(SCD30_CONTINUOUS_MEASUREMENT, 0x0000)
+  def stopMeasurement():
+     writeCommand(SCD30_STOP_MEASUREMENT)
+  def getCarbonDioxideConcentration(*result)  #<----- PUNTEROOO (?)
 
 
