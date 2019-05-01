0f532d3efdaedb7ef15cc233b01a0a089e6ee7a6
BrionesMathias <mathii.briones@gmail.com>
2019-05-01 19:01:36 -0400

    creacion main

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from machine import Pin,I2C
-import SCD30
+import SCD30.SCD30
 
 i2c = I2C(scl=Pin(15), sda=Pin(4), freq=80000)
+SCD30.initialize()
 
 
 while true:
-  SCD30.initialize()
+     result=bytearray(3)
+     if scd30.isAvailable() :
+          scd30.getCarbonDioxideConcentration(result)
+          print("Carbon Dioxide Concentration is: ",result[0],"ppm") 
+          print("Temperature: ",result[1], "°C")
+          print("Humidity:",result[2],"%")
+    time.sleep(2)
+  
+
+
 
